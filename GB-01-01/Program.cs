2600dd1f4a0fd4dc8734dd17351c9a5eadc7a1a0
Artem <a.khimin@yandex.ru>
2021-05-06 01:24:19 +0300

    Добавьте файлы проекта.

---
+++ GB-01-01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GB_01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToLongDateString()}");
+        }
+    }
+}
